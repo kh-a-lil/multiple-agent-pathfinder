727e953ebcd29b1fb7498d3897eb5b10305a97c1
Khalil Raghib <schrodenger99@gmail.com>
2026-04-27 15:35:59 +0100

    check

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,12 @@
+import sys
+from sys import exit
+
+
 def main():
+    if len(sys.argv) != 2:
+        print("usage: uv python3 -m src <input map>")
+        exit(0)
+
     print("Hello from fly-in!")
 
 
