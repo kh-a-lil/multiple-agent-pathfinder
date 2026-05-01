9112efcf162627410d3a2fbf13b1a7c420e6017b
Khalil Raghib <schrodenger99@gmail.com>
2026-05-01 14:01:55 +0100

    check

--- src/space_time_pathfinder.py
+++ src/space_time_pathfinder.py
@@ -4,3 +4,15 @@ from src.data import *
 class PathFinder:
     def __init__(self, map: Graph):
         self.graph: Graph = map
+        self.node_reservations: dict[tuple[str, int], int] = {}
+        self.edge_reservations: dict[tuple[frozenset[str], int], int] = {}
+
+    def djikstra(self):
+        pass
+
+    def looper(self):
+        self.routs = []
+        for drone in range(1, self.graph.nb_drones + 1):
+            rout = self.djikstra()
+            self.routs.append(rout)
+            self.update_reservations(rout)
