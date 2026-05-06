9f190078b33ebcdd9afc658f84cdaabbcc7b6c2a
Khalil Raghib <schrodenger99@gmail.com>
2026-05-06 20:19:04 +0100

    check

--- src/space_time_pathfinder.py
+++ src/space_time_pathfinder.py
@@ -7,40 +7,50 @@ class PathFinder:
         self.graph: Graph = map
         self.node_reservations: dict[tuple[str, int], int] = {}
         self.edge_reservations: dict[tuple[frozenset[str], int], int] = {}
+        self.tea = 0
         self.looper()
 
     def construct_path(self, memory: dict[tuple[str, int], tuple[str, int]]):
         m_len = len(memory) - 1
         path = []
         dest = list(memory.keys())[-1]
-        # path.append(dest)
+        #if self.tea == 24:
+        #    for i, v in memory.items():
+        #        print(i, v)
         while m_len >= 0:
             if list(memory.keys())[m_len] == dest:
                 path.append(dest)
                 dest = list(memory.values())[m_len]
             m_len -= 1
         path.reverse()
+        print(path)
+        self.tea += 1
         return path
 
     def djikstra(self):
         queue: list[(int, Zone, str)] = []
-        visited: list[Zone, int] = []
+        visited: set[str, int] = set()
         memory: dict[tuple[str, int], tuple[str, int]] = {}
         heapq.heappush(queue, (0, self.graph.zones[self.graph.start_hub], None))
         while queue:
             curr_turn, curr_zone, came_from = heapq.heappop(queue)
-            memory[(curr_zone.name, curr_turn)] = (came_from, curr_turn - 1)
+            if came_from == curr_zone.name:
+                memory[(curr_zone.name, curr_turn)] = (came_from, curr_turn - 1)
+            elif curr_zone.zone_type == "restricted":
+                memory[(curr_zone.name, curr_turn)] = (came_from, curr_turn - 2)
+            else:
+                memory[(curr_zone.name, curr_turn)] = (came_from, curr_turn - 1)
             if curr_zone == self.graph.zones[self.graph.end_hub]:
                 return self.construct_path(memory)
-            if (curr_zone, curr_turn) in visited:
+            if (curr_zone.name, curr_turn) in visited:
                 continue
-            visited.append((curr_zone, curr_turn))
+            visited.add((curr_zone.name, curr_turn))
             next_turn: int = curr_turn + 1
             if (
                 self.node_reservations.get((curr_zone.name, next_turn), 0)
                 < curr_zone.max_drones
             ):
-                if (curr_zone, next_turn) in visited:
+                if (curr_zone.name, next_turn) in visited:
                     continue
                 heapq.heappush(queue, (next_turn, curr_zone, curr_zone.name))
             for n in self.graph.adj_list[curr_zone.name]:
@@ -49,7 +59,8 @@ class PathFinder:
                     neighbor.zone_type == ZoneType.NORMAL
                     or neighbor.zone_type == ZoneType.PRIORITY
                 ):
-                    if (neighbor, next_turn) in visited:
+                    next_turn = curr_turn + 1
+                    if (neighbor.name, next_turn) in visited:
                         continue
                     if (
                         self.node_reservations.get((neighbor.name, next_turn), 0)
@@ -57,7 +68,7 @@ class PathFinder:
                     ):
                         if (
                             self.edge_reservations.get(
-                                ((curr_zone.name, neighbor.name), curr_turn), 0
+                                (frozenset([curr_zone.name, neighbor.name]), curr_turn), 0
                             )
                             < self.graph.connections[
                                 frozenset([curr_zone.name, neighbor.name])
@@ -66,7 +77,7 @@ class PathFinder:
                             heapq.heappush(queue, (next_turn, neighbor, curr_zone.name))
                 if neighbor.zone_type == ZoneType.RESTRICTED:
                     next_turn = curr_turn + 2
-                    if (neighbor, next_turn) in visited:
+                    if (neighbor.name, next_turn) in visited:
                         continue
                     if (
                         self.node_reservations.get((neighbor.name, next_turn), 0)
@@ -74,13 +85,13 @@ class PathFinder:
                     ):
                         if (
                             self.edge_reservations.get(
-                                ((curr_zone.name, neighbor.name), curr_turn), 0
+                                (frozenset([curr_zone.name, neighbor.name]), curr_turn), 0
                             )
                             < self.graph.connections[
                                 frozenset([curr_zone.name, neighbor.name])
                             ].max_link_capacity
                             and self.edge_reservations.get(
-                                ((curr_zone.name, neighbor.name), curr_turn + 1), 0
+                                (frozenset([curr_zone.name, neighbor.name]), curr_turn + 1), 0
                             )
                             < self.graph.connections[
                                 frozenset([curr_zone.name, neighbor.name])
@@ -101,7 +112,7 @@ class PathFinder:
         for i in range(1, self.graph.nb_drones + 1):
             rout = self.djikstra()
             self.routs[i] = rout
-            print(rout)
+            #print(rout)
             self.update_reservations(rout)
 
 
